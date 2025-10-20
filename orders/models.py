53cf0993bf139b3205b480080bbe390a98be24e7
shaxweb <igrayunagitare@gmail.com>
2025-10-20 11:43:50 +0000

    Company

--- orders/models.py
+++ orders/models.py
@@ -1,24 +1,19 @@
 from django.conf import settings
 from django.db import models
 
-User = settings.AUTH_USER_MODEL
-
 
 class Order(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	title = models.CharField(max_length=32)
-	description = models.TextField()
-	phone = models.CharField(max_length=20)
-	status = models.CharField(max_length=20, default="Active")
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	
-	def __str__(self):
-		return f"Order {self.title}"
-	
-	class Meta:
-		verbose_name = "Order"
-		verbose_name_plural = "Orders"
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=32)
+    description = models.TextField()
+    phone = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, default="Active")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-		
+    def __str__(self):
+        return f"Order {self.title}"
 
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
